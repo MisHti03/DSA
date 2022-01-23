0e17e3f7d6ce9813a3605f0a42b2ec98314316ef
MisHti03 <84374400+MisHti03@users.noreply.github.com>
2022-01-23 17:28:26 +0530

    Time: 2236 ms (51.69%), Space: 107.3 MB (47.61%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,9 +1,11 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
         adjList=[[] for i in range(n)]
-        for n1,n2 in edges:
-            adjList[n1].append(n2)
-            adjList[n2].append(n1)
+        
+        for v,e in edges:
+            adjList[v].append(e)
+            adjList[e].append(v)
+            
         queue=[start]
         visited=set([start])
         while queue:
